52576fc284470733072a758bc0d25252a9f8f0bc
Vinayak Kalra <vinayak.kalra@quadbtech.com>
2022-07-11 20:20:14 +0530

    Update Airdrop.sol

--- Airdrop.sol
+++ Airdrop.sol
@@ -5,11 +5,18 @@ pragma solidity >=0.7.0 <0.9.0;
 contract Airdrop {
 
     mapping ( address => bool) airdropAddresses;
-    uint256 airdrop_amount = 10 * 10**18; // equivalent to 10 klay
+    uint256 airdrop_amount = 1 * 10**18; // equivalent to 10 klay
+    address owner;
+    uint256 balance = 0;
+
+    constructor() {
+      owner = msg.sender;
+    }
 
     function airdrop() public {
-        if(airdropAddresses[msg.sender] == false){
+        if(airdropAddresses[msg.sender] == false && balance >= airdrop_amount){
             payable(msg.sender).transfer(airdrop_amount);
+            balance = balance - airdrop_amount;
             airdropAddresses[msg.sender] = true;
         }
     }
@@ -17,4 +24,18 @@ contract Airdrop {
     function hasAirdropped(address _address) public view returns(bool){
         return airdropAddresses[_address];
     }
+
+    modifier onlyOwner {
+      require(msg.sender == owner);
+      _;
+    }
+
+    function deposit() onlyOwner public payable {
+        balance = balance + msg.value;
+    }
+
+    function getBalance() public view returns(uint256){
+        return balance;
+    }
+
 }
